87e0ada85fb5f833ee6caa900609840f99556f93
artakak <a260641139@ya.ru>
2018-04-20 11:55:41 +0300

    st

--- RESTfullRobo.py
+++ RESTfullRobo.py
@@ -5,18 +5,18 @@ app = Flask(__name__)
 api = Api(app)
 
 
-class HelloWorld(Resource):
+class Status(Resource):
     def get(self):
         return {'hello': 'world'}
 
 
-class Switch1(Resource):
-    def post(self, state):
+class Command(Resource):
+    def post(self, command):
         print(request.json)
-        return {'switch': str(state)}
+        return {'command': str(command)}
 
 
-api.add_resource(HelloWorld, '/')
-api.add_resource(Switch1, '/switch<int:state>')
+api.add_resource(Status, '/status')
+api.add_resource(Command, '/command/<string:command>')
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
